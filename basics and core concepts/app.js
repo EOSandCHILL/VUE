692f32b974c720eb71f1b612cc03d1cbef6d3a7f
MackInTech <jerrykmack803@yahoo.com>
2023-01-23 12:45:20 -0500

    data binding practice

--- basics and core concepts/app.js
+++ basics and core concepts/app.js
@@ -1,10 +1,20 @@
 const app = Vue.createApp({
   data() {
     return {
-      courseGoal: "Finished the course and learned VUE",
+      courseGoalA: "Finished the course and learned Vue!",
+      courseGoalB: "Master Vue and build amazing apps!",
       vueLink: "https://vuejs.org",
     };
   },
+  methods: {
+    outputGoal() {
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        return this.courseGoalA;
+      } else {
+        return this.courseGoalB;
+      }
+    },
+  },
 });
-
 app.mount("#user-goal");

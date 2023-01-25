d31d0c395c7074ac5068de6eded20b2ce8643be7
MackInTech <jerrykmack803@yahoo.com>
2023-01-25 13:18:36 -0500

    watchers

---
+++ basics 5/app.js
@@ -0,0 +1,61 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name: "",
+      lastName: "",
+      // fullName: "",
+    };
+  },
+  watch: {
+    counter(value) {
+      if (value > 50 || value < -50) {
+        this.counter = 0;
+        // good use for a watcher
+      }
+    },
+    // name(value) {
+    //   if (value === "") {
+    //     this.fullName = "";
+    //   } else {
+    //     this.fullName = value + " " + this.lastName;
+    //   }
+    // },
+    // lastName(value) {
+    //   if (value === "") {
+    //     this.fullName = "";
+    //   } else {
+    //     this.fullName = this.name + " " + value;
+    //   }
+    // },
+  },
+  computed: {
+    fullName() {
+      console.log("Running again...");
+      if (this.name === "" || this.lastName === "") {
+        return "";
+      }
+      return this.name + " " + this.lastName;
+    },
+  },
+  methods: {
+    outputFullname() {
+      return this.name + " " + "Mack";
+    },
+    setName(event) {
+      this.name = event.target.value;
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
+      // this.counter--;
+    },
+    resetInput() {
+      this.name = "";
+    },
+  },
+});
+
+app.mount("#events");
